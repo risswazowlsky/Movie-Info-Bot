a996fcc90d3253568cf1e335121c7a979082d062
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-11-30 14:09:45 +0530

    Update main file

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
-
 import os
 from pyrogram import Client
+
 
 bot_token = os.environ["BOT_TOKEN"]
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
-
-plugins = dict(
-    root="plugins"
-)
+plugins = dict(root="plugins")
 
 Bot = Client(
     "Movie-Info-Bot",
